7942ed40074da542e03d7db545a91cab7504efa4
Andy Zhuo <azhuo@college.harvard.edu>
2023-01-06 21:28:55 +0100

    Updates

---
+++ precache-manifest.331eb1205e49e33a42811f2dd8e6965e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "231603be520797209433",
+    "url": "/static/css/main.8c88a1e4.chunk.css"
+  },
+  {
+    "revision": "231603be520797209433",
+    "url": "/static/js/main.231603be.chunk.js"
+  },
+  {
+    "revision": "69085753fa23843624a5",
+    "url": "/static/css/1.eed8ebc3.chunk.css"
+  },
+  {
+    "revision": "69085753fa23843624a5",
+    "url": "/static/js/1.69085753.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1cf2aba2d9c71bb41e9b9f2013c1ceb4",
+    "url": "/static/media/Zhuo, Andy - Resume.1cf2aba2.pdf"
+  },
+  {
+    "revision": "69e325daf139c265f39192bc628c4bc4",
+    "url": "/static/media/profile.69e325da.png"
+  },
+  {
+    "revision": "1b3ec22b93d80a8cf583d65a46f7dd88",
+    "url": "/static/media/proto.1b3ec22b.png"
+  },
+  {
+    "revision": "cd26ad26bdff184b4fd1c803a9ae682f",
+    "url": "/static/media/USAIDproto.cd26ad26.png"
+  },
+  {
+    "revision": "c3fef32461801e32d6a617980e56730c",
+    "url": "/static/media/CIDI.c3fef324.png"
+  },
+  {
+    "revision": "a02150d601bd8bb1e1705601d2eae0f6",
+    "url": "/static/media/autodiff.a02150d6.png"
+  },
+  {
+    "revision": "36949093897e56ce9795d6fba8a5ec06",
+    "url": "/static/media/crimsonconnect.36949093.png"
+  },
+  {
+    "revision": "32b917427c8cab3238942400b760500a",
+    "url": "/static/media/echo.32b91742.png"
+  },
+  {
+    "revision": "67861fa6e882921e8cc6997f0fbd5f17",
+    "url": "/static/media/deluge.67861fa6.png"
+  },
+  {
+    "revision": "1cec33c8adcf6a4f36aed2427ff682d2",
+    "url": "/static/media/music.1cec33c8.png"
+  },
+  {
+    "revision": "723e56cf38f670b4cf26a8e79a98c2e4",
+    "url": "/static/media/starcolor.723e56cf.png"
+  },
+  {
+    "revision": "31e34a4dc526ffb104e2d988f6f52516",
+    "url": "/static/media/Montserrat-SemiBold.31e34a4d.ttf"
+  },
+  {
+    "revision": "5f797490f806b3b229299f0a66de89c9",
+    "url": "/static/media/Montserrat-Medium.5f797490.ttf"
+  },
+  {
+    "revision": "a8de8ffe262db7e277deb8f1c439c518",
+    "url": "/static/media/Montserrat-Bold.a8de8ffe.ttf"
+  },
+  {
+    "revision": "9e52b00d737492e337b2498406aef98f",
+    "url": "/static/media/Montserrat-Light.9e52b00d.ttf"
+  },
+  {
+    "revision": "f7213526ec9296ff43426bfe3eae8926",
+    "url": "/static/media/Montserrat-Regular.f7213526.ttf"
+  },
+  {
+    "revision": "eab379069643b31bfb0457a0896c8387",
+    "url": "/static/media/lowellgo.eab37906.png"
+  },
+  {
+    "revision": "94a197e0eaf2b2412f4327aad6551345",
+    "url": "/static/media/mslogo.94a197e0.png"
+  },
+  {
+    "revision": "bbc89764d86317669a04cf8bfc431e40",
+    "url": "/index.html"
+  }
+];
